ec89b6012760c350f48691f3d671ff4144b30dcf
Skote616 <kupoles09@gmail.com>
2026-03-11 14:56:26 +0500

    triangletestcorrect

--- TriangleTest/obj/Debug/net8.0/TriangleTest.AssemblyInfo.cs
+++ TriangleTest/obj/Debug/net8.0/TriangleTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TriangleTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86c07965bcb1fea66651dda791a37218274fdd1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45718ed08655e472b47841d8b22e85158e31c806")]
 [assembly: System.Reflection.AssemblyProductAttribute("TriangleTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TriangleTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
